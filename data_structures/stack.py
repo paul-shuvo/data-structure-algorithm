96ec0eb2f8bf9c1e8f9b087431de8203a058fbae
Shuvo Kumar Paul <shuvo.k.paul@gmail.com>
2020-05-29 13:56:40 -0700

    feat: pop and peek method added

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -2,12 +2,26 @@ class Node:
     def __init__(self, data=None):
         self.data = data
         self.next = None
+    
+    def __str__(self):
+        return str(self.data)
         
 class Stack:
     def __init__(self, size):
         self.head = None
         self.size = size
         self.length = 0
+    
+    def __len__(self):
+        return self.length
+    
+    def __str__(self):
+        node = self.head
+        retval = ""
+        while node is not None:
+            retval += str(node) + ','
+            node = node.next
+        return retval[:-1]
     
     def is_empty(self):
         return True if self.length == 0 else False
@@ -22,4 +36,32 @@ class Stack:
         
         node.next = self.head
         self.head = node
+        self.length += 1
         
+    def pop(self):
+        if self.is_empty():
+            raise IndexError('Stack is full')
+        self.head = self.head.next
+        self.length -= 1
+    
+    def peek(self, index):
+        if index >= self.length:
+            raise IndexError(f'index: {index} greater than the stack length: {self.length}')
+        count = self.length - 1
+        node = self.head
+        while count >= index:
+            if count == index:
+                return node.data
+            count -= 1
+            node = node.next
+            
+# stk = Stack(5)
+# stk.push(1)
+# stk.push("hee")
+# stk.push(12.45)
+# print(stk)
+# # stk.push(stk)
+# stk2 = Stack(3)
+# stk2.push(5)
+# stk2.push(stk)
+# print(stk2)
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -30,12 +30,49 @@ def test_push():
     
     stk.push(2)
     assert stk.head.data == 2
-    assert stk.head.next.data = 2
+    assert stk.head.next.data == 1
     
     stk.push(3)
     assert stk.head.data == 3
-    assert stk.head.next.data == 3
-    assert stk.head.next.next.data == 3
+    assert stk.head.next.data == 2
+    assert stk.head.next.next.data == 1
     
     with pytest.raises(IndexError):
         stk.push(4)
+        
+def test_pop():
+    stk = Stack(0)
+    with pytest.raises(IndexError):
+        stk.pop()
+    
+    stk = Stack(3)
+    stk.push(1)
+    stk.push(2)
+    stk.push(3)
+    stk.pop()
+    stk.pop()
+    assert stk.head.data == 1
+    assert stk.length == 1
+    
+def test_str():
+    stk = Stack(3)
+    stk.push(1)
+    stk.push(2)
+    stk.push(3)
+    
+    assert str(stk) == '3,2,1'
+    
+def test_peek():
+    stk = Stack(5)
+    stk.push(0)
+    stk.push(1)
+    stk.push(2)
+    stk.push(3)
+    stk.push(4)
+    
+    with pytest.raises(IndexError):
+        stk.peek(5)
+    
+    assert stk.peek(0) == 0
+    assert stk.peek(2) == 2
+    assert stk.peek(4) == 4
